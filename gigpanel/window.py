6877d31fd8320f351b426315b774484a3b46608c
Martin Spinler <martin.spinler@gmail.com>
2023-09-07 14:45:05 +0200

    filename change

--- gigpanel/window.py
+++ gigpanel/window.py
@@ -74,7 +74,7 @@ def song_update_path(song):
         song['filename'] = None
 
     if (song['filename'] is None or not QFile(song['filename']).exists()) and 'pattern' in store:
-        for fn in ([song['file']] if 'file' in song else []) + [song['name']]:
+        for fn in ([song['file']] if 'file' in song and song['file'] else []) + [song['name']]:
             for instrument in ['-Piano', ' - Piano', '-Electric_Piano', '']:
                 filename = app.config['prefixes'][store['prefix']] + store['pattern'].format(name=fn, instrument=instrument)
                 if QFile(filename).exists():
