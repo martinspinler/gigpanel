34e47c77cfcf467fa7c79e3e38d4d47899fad714
Martin Spinler <martin.spinler@gmail.com>
2023-11-26 14:01:36 +0100

    move songlist to widgets

--- gigpanel/widgets/__init__.py
+++ gigpanel/widgets/__init__.py
@@ -1,3 +1,4 @@
 from .docwidget import DocumentWidgetScrollArea, DocumentWidget
+from .songlist import SongListDialog
 
-__all__ = ["DocumentWidgetScrollArea", "DocumentWidget"]
+__all__ = ["DocumentWidgetScrollArea", "DocumentWidget", "SongListDialog"]
